90758cb542db658e4df398bb098eca1bb21bd4c4
yagna-muni <51692887+yagna-muni@users.noreply.github.com>
2023-09-09 00:49:48 -0400

    Delete Design Patterns 5 directory

--- Design Patterns 5/src/edu/umb/cs680/hw05/Authenticator.java
+++
@@ -1,8 +0,0 @@
-package edu.umb.cs680.hw05;
-
-public class Authenticator {
-    public static boolean authenticate(SecurityContext context , EncryptedString password){
-        return true;
-    };
-
-}
--- Design Patterns 5/src/edu/umb/cs680/hw05/State.java
+++
@@ -1,6 +0,0 @@
-package edu.umb.cs680.hw05;
-
-public interface State {
-    public void login(EncryptedString pwd);
-    public void logout();
-}
